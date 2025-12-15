f170520d878bfe76af9095120377f12db87a7c6f
Guizaoo <guilhermexxzz54@gmail.com>
2025-12-15 14:22:46 -0300

    tela de adm

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-blue-900 text-center size-20px'>
+      <h1>oi</h1>
+    </div>
+  )
+}
+
+export default App
